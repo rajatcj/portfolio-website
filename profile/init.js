0bb4bfb71ce7deddf6083cdbfb695b9e576d3091
Rajat Jha <rcjha23@gmail.com>
2022-11-22 02:35:09 +0545

    a

--- profile/init.js
+++ profile/init.js
@@ -1,3 +1,5 @@
+
+
 // values to change for the height of the activities rectangle
 var originalh = -0.2;
 var addedh = 5;
@@ -26,9 +28,32 @@ async function init(data) {
                 div.innerHTML = '<img draggable="false" alt="" width="80" height="80" src="' +
                     json.spotify['album_art_url'] + '"> ' +"<ul><li><strong>" + 'LIlSTENING TO SPOTIFY...' + "</strong></li>"  + "<li>" +
                     songinfo.join("</li><li>") + '</li></ul>';
-            };
+            } else {
+                // time elapsed timer
+                const current_time = element.timestamps['start'],
+                exp_time = Math.floor(Date.now() / 1000)
+                diff = (exp_time * 1000) - current_time,
+                formatTime = (ms) => {
+                    const seconds = Math.floor((ms / 1000) % 60);
+                    const minutes = Math.floor((ms / 1000 / 60) % 60);
+                    const hours = Math.floor((ms / 1000 / 3600) % 60);
+                    return [hours, minutes, seconds].map(v => String(v).padStart(2,0)).join(':');
+                }
+                var activityinfo = ["<strong>" + element['name'] + "</strong>", + (element['details'] === undefined ? "" : "<p>" + element['details'] + "</p>"), + (element['state'] === undefined ? "" : "<p>" + element['state'] + "</p>"), "<p>" + formatTime(diff) + " elapsed </p>"]
+                if(element.assets !== undefined) {
+                    div.innerHTML = ('<img draggable="false" alt="" onerror=this.src="https://cdn.discordapp.com/app-assets/' +
+                        element['application_id'] + '/' + element.assets['large_image'] +
+                        '.png" width="64" height="64" src="https://cdn.discordapp.com/app-assets/' + userid + '/' +
+                        element.assets['large_image'] + '.png"> <div class="other">' +
+                        "<ul><li>" + activityinfo.join("</li><li>") + "</li></ul>" + '</div>');
+                } else if(element.assets === undefined) {
+                    div.innerHTML = ('<img draggable="false" alt="" width="64" height="64" src="https://cdn.discordapp.com/emojis/932294894775599174.gif"> <div class="other">' +
+                        "<ul><li>" + activityinfo.join("</li><li>") + "</li></ul>" + '</div>');
+                }
+            }
+        
 
-            
+            h += addedh;
 
             currentdiv.appendChild(div);
         } else {
@@ -38,7 +63,8 @@ async function init(data) {
         }
     });
 
-    
+    // set height of activities rect
+    currentdiv.style.height = h + "rem";
 }
 
 async function updatepresence() {
@@ -64,12 +90,50 @@ async function updatepresence() {
             
             // if the activity is spotify try to get all of the song info instead of the activity details
             if(element['name'] === "Spotify") {
-                var songinfo = [json.spotify['song'], "by " + json.spotify['artist'].split('; ').join(', '), "on " + json.spotify['album']]
+                const start_time = element.timestamps['start']
+                const end_time = element.timestamps['end']
+                cur_time = Math.floor(Date.now() / 1000)
+
+                now = (cur_time * 1000) - start_time,
+                end = end_time - start_time,
+
+                playTime = (ms) => {
+                    const seconds = Math.floor((ms / 1000) % 60);
+                    const minutes = Math.floor((ms / 1000 / 60) % 60);
+                    const hours = Math.floor((ms / 1000 / 3600) % 60);
+                    return [minutes, seconds].map(v => String(v).padStart(2,0)).join(':');
+                }
+                
+                
+                var songinfo = [json.spotify['song'], playTime(now) + " -------- > -------- " + playTime(end), "by " + json.spotify['artist'].split('; ').join(', '), "on " + json.spotify['album']]
                 div.innerHTML = '<img draggable="false" alt="" width="80" height="80" src="' +
-                    json.spotify['album_art_url'] + '"> ' +"<ul><li><strong>" + 'LISTENING TO SPOTIFY...' + "</strong></li>"  + "<li>" +
+                    json.spotify['album_art_url'] + '"> ' +"<ul><li>" + 'LISTENING TO SPOTIFY...' + "</li>"  + "<li>" +
                     songinfo.join("</li><li>") + '</li></ul>';
-            } 
-            
+            } else {
+                // time elapsed timer
+                const current_time = element.timestamps['start'],
+                exp_time = Math.floor(Date.now() / 1000)
+                diff = (exp_time * 1000) - current_time,
+                formatTime = (ms) => {
+                    const seconds = Math.floor((ms / 1000) % 60);
+                    const minutes = Math.floor((ms / 1000 / 60) % 60);
+                    const hours = Math.floor((ms / 1000 / 3600) % 60);
+                    return [hours, minutes, seconds].map(v => String(v).padStart(2,0)).join(':');
+                }
+                
+                var activityinfo = ["<li id='type'>" + (element['type'] === 0 ? "PLAYING..." : (element['type'] === 1 ? "STREAMING..." : (element['type'] === 2 ? "LISTENING..." : (element['type'] === 3 ? "WATCHING..." : "")))) + "</li><li id='name'>" + element['name'] + "</li>", (element['details'] === undefined ? "" : "<li id='details'>" +  element['details'] + "</li>"), (element['state'] === undefined ? "" : "<li id='state'>" + element['state'] + "</li>"), "<li id='time'>" + formatTime(diff) + " elapsed </li>"]
+                if(element.assets !== undefined) {
+                    div.innerHTML = ('<img draggable="false" alt="" onerror=this.src="https://cdn.discordapp.com/app-assets/' +
+                        element['application_id'] + '/' + element.assets['large_image'] +
+                        '.png" width="80" height="80" src="https://cdn.discordapp.com/app-assets/'+ userid + '/' +
+                        element.assets['large_image'] + '.png"> <div class="other">' +
+                        "<ul>" + activityinfo.join("") + "</ul>" + '</div>');
+                } else if(element.assets === undefined) {
+                    div.innerHTML = ('<img draggable="false" alt="" width="80" height="80" src="unknown.png"> <div class="other">' +
+                        "<ul>" + activityinfo.join("") + "</ul>" + '</div>');
+                }
+            }
+            h += addedh;
 
             if(exists)
                 currentdiv.appendChild(div);
@@ -90,6 +154,8 @@ async function updatepresence() {
     var toremove = childnames.filter(x => !names.includes(x));
     children.filter(x => toremove.includes(x.id)).forEach(e => {e.remove()});
 
+    // set height of activities rect
+    currentdiv.style.height = h + "rem";
 }
 
 const onload = async () => {
