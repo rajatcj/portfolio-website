4bc8ec118db1bdc3a1fa0ca2930d99a5f579f5fd
Rajat Jha <rcjha23@gmail.com>
2022-11-22 20:59:25 +0545

    a

--- profile/init.js
+++ profile/init.js
@@ -4,85 +4,8 @@
 var originalh = -0.2;
 var addedh = 5;
 
-// change userId to your user ID
-var userid = "754033245972201612";
 
-// different from updatepresence, adds the username, pfp, status, original activities
-async function init() {
-    let json = await lanyard({userId: userid});
-    let activities = json.activities;
-    let currentdiv = document.getElementById("activities");
-    var h = originalh;
-    let curractiv = 1;
-    activities.forEach(element => {
-        // if not the status activity, continue
-        if(element['type'] !== 4) {
-            var div = document.createElement("div");
-            div.id = element['name'].split(' ').join('').toLowerCase();
-            div.className = "activity";
-            // if the activity is spotify try to get all of the song info instead of the activity details
-            if(element['name'] === "Spotify") {
-                const start_time = element.timestamps['start']
-                const end_time = element.timestamps['end']
-                cur_time = Math.floor(Date.now() / 1000)
-
-                now = (cur_time * 1000) - start_time,
-                end = end_time - start_time,
-
-                playTime = (ms) => {
-                    const seconds = Math.floor((ms / 1000) % 60);
-                    const minutes = Math.floor((ms / 1000 / 60) % 60);
-                    const hours = Math.floor((ms / 1000 / 3600) % 60);
-                    return [minutes, seconds].map(v => String(v).padStart(2,0)).join(':');
-                }
-
-                percent = (now/end)*100;
-                
-                var songinfo = ["<li id='spotify-songname'>" + json.spotify['song'] + "</li>", "<li id='spotify-bar'><div class='progress-bar'><div class='fill' style='width: " + percent + "%;'></div></div></li>", "<li id='spotify-time'>" + playTime(now) + "/" + playTime(end) + "</li>", "<li id='spotify-artist'>" + "by " + json.spotify['artist'].split('; ').join(', ') + "</li>", "<li id='spotify-album'>" + "on " + json.spotify['album'] + "</li>"]
-                
-                div.innerHTML = '<img draggable="false" alt="" width="80" height="80" src="' +
-                    json.spotify['album_art_url'] + '"> ' +"<ul><li id='spotify-ing'>" + 'LISTENING TO SPOTIFY...' + "</li>" +
-                    songinfo.join("") + '</ul>';
-            } else {
-                // time elapsed timer
-                current_time = (element.timestamps['start'] === undefined ? element['created_at'] : element.timestamps['start']),
-                exp_time = Math.floor(Date.now() / 1000),
-                diff = (exp_time * 1000) - current_time,
-                formatTime = (ms) => {
-                    const seconds = Math.floor((ms / 1000) % 60);
-                    const minutes = Math.floor((ms / 1000 / 60) % 60);
-                    const hours = Math.floor((ms / 1000 / 3600) % 60);
-                    return [hours, minutes, seconds].map(v => String(v).padStart(2,0)).join(':');
-                }
-                var activityinfo = ["<strong>" + element['name'] + "</strong>", + (element['details'] === undefined ? "" : "<p>" + element['details'] + "</p>"), + (element['state'] === undefined ? "" : "<p>" + element['state'] + "</p>"), "<p>" + formatTime(diff) + " elapsed </p>"]
-                if(element.assets !== undefined) {
-                    div.innerHTML = ('<img draggable="false" alt="" onerror=this.src="https://cdn.discordapp.com/app-assets/' +
-                        element['application_id'] + '/' + element.assets['large_image'] +
-                        '.png" width="64" height="64" src="https://cdn.discordapp.com/app-assets/' + userid + '/' +
-                        element.assets['large_image'] + '.png"> <div class="other">' +
-                        "<ul><li>" + activityinfo.join("</li><li>") + "</li></ul>" + '</div>');
-                } else if(element.assets === undefined) {
-                    div.innerHTML = ('<img draggable="false" alt="" width="64" height="64" src="https://cdn.discordapp.com/emojis/932294894775599174.gif"> <div class="other">' +
-                        "<ul><li>" + activityinfo.join("</li><li>") + "</li></ul>" + '</div>');
-                }
-            }
-        
-
-            h += addedh;
-
-            currentdiv.appendChild(div);
-        } else {
-            // if it is the status, set the src of the emoji img and the status text itself
-            document.getElementById("statusemoji").src = "https://cdn.discordapp.com/emojis/" + element.emoji['id'] + (element.emoji['animated'] ? ".gif" : ".png");
-            document.getElementById("status").innerHTML = element['state'];
-        }
-    });
-
-    // set height of activities rect
-    currentdiv.style.height = h + "rem";
-}
-
-async function updatepresence() {
+async function updatepresence(userid) {
     var json = await lanyard({userId: userid});
     let activities = json.activities;
     let currentdiv = document.getElementById("activities");
