f3a8b5ee13a8fbd45df9cc6a65524f3ceb2c6a09
Rajat Jha <rcjha23@gmail.com>
2022-11-24 18:26:27 +0545

    a

--- profile/assets/javascripts/activity.js
+++ profile/assets/javascripts/activity.js
@@ -1,5 +1,3 @@
-
-
 // values to change for the height of the activities rectangle
 var originalh = -0.2;
 var addedh = 5;
@@ -28,7 +26,7 @@ async function updatepresence(userid) {
             
             // if the activity is spotify try to get all of the song info instead of the activity details
             if(element['name'] === "Spotify") {
-                const start_time = (element.timestamps['start'] === undefined ? element['created_at'] : element.timestamps['start'])
+                const start_time = element.timestamps['start']
                 const end_time = element.timestamps['end']
                 cur_time = Math.floor(Date.now() / 1000)
 
@@ -46,11 +44,12 @@ async function updatepresence(userid) {
                 
                 var songinfo = ["<li id='spotify-songname'>" + json.spotify['song'] + "</li>", "<li id='spotify-bar'><div class='progress-bar'><div class='fill' style='width: " + percent + "%;'></div><p id='spotify-time'>" + playTime(now) + "/" + playTime(end) + "</p></div></li>", "<li id='spotify-artist'>" + "by " + json.spotify['artist'].split('; ').join(', ') + "</li>", "<li id='spotify-album'>" + "on " + json.spotify['album'] + "</li>"]
                 
-                div.innerHTML = '<div class="assets-images"><img  class="spotify-thumbnail" onerror=this.src="unknown.png" width="80" height="80" src="' + 
-                json.spotify['album_art_url'] + '">' + '<a href="https://open.spotify.com/track/' + json.spotify['track_id'] + '" target="_blank"> <img class="spotifylink-image" width="45" height="45" src="spotifylink.gif"></a>' + '</div>' + '<ul><li id="spotify-ing">' + 'LISTENING TO SPOTIFY...' + "</li>" +
+                div.innerHTML = '<div class="assets-images"><img  class="spotify-thumbnail" onerror=this.src="./assets/img/unknown.png" width="80" height="80" src="' + 
+                json.spotify['album_art_url'] + '">' + '<a href="https://open.spotify.com/track/' + json.spotify['track_id'] + '" target="_blank"> <img class="spotifylink-image" width="45" height="45" src="./assets/img/spotifylink.gif"></a>' + '</div>' + '<ul><li id="spotify-ing">' + 'LISTENING TO SPOTIFY...' + "</li>" +
                     songinfo.join("") + '</ul>';
             } else {
-                current_time = element.timestamps['start'],
+                const current_time = (element.hasOwnProperty("timestamps") === false ? element['created_at'] : (element.timestamps['start'] === undefined ? element['created_at'] : element.timestamps['start'])) 
+                
                 exp_time = Math.floor(Date.now() / 1000),
                 diff = (exp_time * 1000) - current_time,
                 formatTime = (ms) => {
@@ -62,12 +61,12 @@ async function updatepresence(userid) {
                 
                 var activityinfo = ["<li id='type'>" + (element['application_id'] === "920907514344779827" ? "ACTIVE ON..." : (element['type'] === 0 ? "PLAYING..." : (element['type'] === 1 ? "STREAMING..." : (element['type'] === 2 ? "LISTENING..." : (element['type'] === 3 ? "WATCHING..." : ""))))) + "</li><li id='name'>" + element['name'] + "</li>", (element['details'] === undefined ? "" : "<li id='details'>" +  element['details'] + "</li>"), (element['state'] === undefined ? "" : "<li id='state'>" + element['state'] + "</li>"), "<li id='time'>" + formatTime(diff) + " elapsed </li>"]
                 if(element.assets !== undefined) {
-                    div.innerHTML = ('<div class="assets-images"><img  class="large-image" onerror=this.src="unknown.png" width="80" height="80" src="https://cdn.discordapp.com/app-assets/'+ element['application_id'] + '/' +
+                    div.innerHTML = ('<div class="assets-images"><img  class="large-image" onerror=this.src="./assets/img/unknown.png" width="80" height="80" src="https://cdn.discordapp.com/app-assets/'+ element['application_id'] + '/' +
                         element.assets['large_image'] + '.png">' + (element.assets['small_image'] === undefined ? "" : '<img class="small-image" width="25" height="25" src="https://cdn.discordapp.com/app-assets/'+ element['application_id'] + '/' +
                         element.assets['small_image'] + '.png">') + '</div><div class="other">' +
                         "<ul>" + activityinfo.join("") + "</ul>" + '</div>');
                 } else if(element.assets === undefined) {
-                    div.innerHTML = ('<img draggable="false" alt="" width="80" height="80" src="unknown.png"> <div class="other">' +
+                    div.innerHTML = ('<img draggable="false" alt="" width="80" height="80" src="./assets/img/unknown.png"> <div class="other">' +
                         "<ul>" + activityinfo.join("") + "</ul>" + '</div>');
                 }
             }
