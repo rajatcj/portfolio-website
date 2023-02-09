bbe7597bfeb63891437f446839c43fb738334ad7
Rajat Jha <rcjha23@gmail.com>
2023-02-10 00:59:58 +0545

    new profile

---
+++ profile-old/assets/javascripts/activity.js
@@ -0,0 +1,110 @@
+// values to change for the height of the activities rectangle
+var originalh = -0.2;
+var addedh = 5;
+
+
+
+function updatepresenceLoop() {
+  theTimeLooper = setInterval(() => {
+    var json = JSON.parse(sessionStorage.getItem("activityData"));
+    let activities = json.activities;
+    let currentdiv = document.getElementById("activities");
+    var h = originalh;
+    activities.forEach(element => {
+        // if not the status activity, continue
+        if(element['type'] !== 4) {
+            var activityname = element['name'].split(' ').join('').toLowerCase();
+            var exists = true;
+            if(document.getElementById(activityname) !== null)
+                exists = document.getElementById(activityname)['length'] == 0;
+            let div = document.getElementById(activityname);
+
+            // check if the activity already exists, if it does just modify the existing one to not create multiple instances
+            if(exists) {
+                div = document.createElement("div");
+                div.id = activityname;
+                div.className = "activity";
+            }
+            
+            // if the activity is spotify try to get all of the song info instead of the activity details
+            if(element['name'] === "Spotify") {
+                const start_time = element.timestamps['start']
+                const end_time = element.timestamps['end']
+                cur_time = Math.floor(Date.now() / 1000)
+
+                now = (cur_time * 1000) - start_time,
+                end = end_time - start_time,
+
+                playTime = (ms) => {
+                    const seconds = Math.floor((ms / 1000) % 60);
+                    const minutes = Math.floor((ms / 1000 / 60) % 60);
+                    const hours = Math.floor((ms / 1000 / 3600) % 60);
+                    return [minutes, seconds].map(v => String(v).padStart(2,0)).join(':');
+                }
+
+                percent = (now/end)*100;
+                
+                var songinfo = ["<li id='spotify-songname'>" + json.spotify['song'] + "</li>", "<li id='spotify-bar'><div class='progress-bar'><div class='fill' style='width: " + percent + "%;'></div><p id='spotify-time'>" + playTime(now) + "/" + playTime(end) + "</p></div></li>", "<li id='spotify-artist'>" + "by " + json.spotify['artist'].split('; ').join(', ') + "</li>", "<li id='spotify-album'>" + "on " + json.spotify['album'] + "</li>"]
+                
+                div.innerHTML = '<div class="assets-images"><img  class="spotify-thumbnail" onerror=this.src="./assets/img/unknown.png" width="80" height="80" src="' + 
+                json.spotify['album_art_url'] + '">' + '<a href="https://open.spotify.com/track/' + json.spotify['track_id'] + '" target="_blank"> <img class="spotifylink-image" width="45" height="45" src="./assets/img/spotifylink.gif"></a>' + '</div>' + '<ul><li id="spotify-ing">' + 'LISTENING TO SPOTIFY...' + "</li>" +
+                    songinfo.join("") + '</ul>';
+            } else {
+                const current_time = (element.hasOwnProperty("timestamps") === false ? element['created_at'] : (element.timestamps['start'] === undefined ? element['created_at'] : element.timestamps['start'])) 
+                
+                exp_time = Math.floor(Date.now() / 1000),
+                diff = (exp_time * 1000) - current_time,
+                formatTime = (ms) => {
+                    const seconds = Math.floor((ms / 1000) % 60);
+                    const minutes = Math.floor((ms / 1000 / 60) % 60);
+                    const hours = Math.floor((ms / 1000 / 3600) % 60);
+                    return [hours, minutes, seconds].map(v => String(v).padStart(2,0)).join(':');
+                }
+                
+                var activityinfo = ["<li id='type'>" + (element['application_id'] === "920907514344779827" ? "ACTIVE ON..." : (element['type'] === 0 ? "PLAYING..." : (element['type'] === 1 ? "STREAMING..." : (element['type'] === 2 ? "LISTENING..." : (element['type'] === 3 ? "WATCHING..." : ""))))) + "</li><li id='name'>" + element['name'] + "</li>", (element['details'] === undefined ? "" : "<li id='details'>" +  element['details'] + "</li>"), (element['state'] === undefined ? "" : "<li id='state'>" + element['state'] + "</li>"), "<li id='time'>" + formatTime(diff) + " elapsed </li>"]
+                if(element.assets !== undefined) {
+                    div.innerHTML = ('<div class="assets-images"><img  class="large-image" onerror=this.src="./assets/img/unknown.png" width="80" height="80" src="https://cdn.discordapp.com/app-assets/'+ element['application_id'] + '/' +
+                        element.assets['large_image'] + '.png">' + (element.assets['small_image'] === undefined ? "" : '<img class="small-image" width="25" height="25" src="https://cdn.discordapp.com/app-assets/'+ element['application_id'] + '/' +
+                        element.assets['small_image'] + '.png">') + '</div><div class="other">' +
+                        "<ul>" + activityinfo.join("") + "</ul>" + '</div>');
+                } else if(element.assets === undefined) {
+                    div.innerHTML = ('<img draggable="false" alt="" width="80" height="80" src="./assets/img/unknown.png"> <div class="other">' +
+                        "<ul>" + activityinfo.join("") + "</ul>" + '</div>');
+                }
+            }
+            h += addedh;
+
+            if(exists)
+                currentdiv.appendChild(div);
+        }
+    });
+
+    // get the difference of the current activities and the last, mostly just to remove activities that aren't active anymore
+    let names = [];
+    activities.forEach(e => {if(e['type'] !== 4)names.push(e['name'].split(' ').join('').toLowerCase())})
+    var children = [].slice.call(currentdiv.getElementsByClassName('activity'), 0);
+    var childnames = new Array(children.length);
+    var array1Length = children.length;
+    var array2Length = names.length;
+    for (var i = 0; i < array1Length; i++) {
+        var name = children[i].getAttribute("id");    
+        childnames[i] = name;
+    }
+    var toremove = childnames.filter(x => !names.includes(x));
+    children.filter(x => toremove.includes(x.id)).forEach(e => {e.remove()});
+
+    // set height of activities rect
+    currentdiv.style.height = h + "rem";
+
+
+  }, 1000);
+
+
+}
+
+
+
+
+
+
+
---
+++ profile/README.md
@@ -0,0 +1,52 @@
+<body style="width: 100%; height: 100%">
+  
+  <h1 align="center">Discord User Card - New version</h1>
+  <p align="center">
+    <a target='_blank' href='https://developer.mozilla.org/fr/'><img src="https://forthebadge.com/images/badges/uses-html.svg"></a>&nbsp<a target='_blank' href='https://developer.mozilla.org/fr/'><img src="https://forthebadge.com/images/badges/uses-css.svg"></a>&nbsp<a target='_blank' href='https://developer.mozilla.org/fr/'><img src="https://forthebadge.com/images/badges/uses-js.svg"></a>
+  </p>
+</body>
+
+## NEW REALEASE :
+
+A version of this card with spotify live activity is available here : <a href="https://github.com/Sybrax/Discord_User_Card_Spotify_App" target="_blank">check it out !</a> !
+
+## Description :
+
+The card when you click on a user in a discord server.
+
+- Banner & Logo
+- Badges with tooltip
+- Buttons
+- Username
+- About Me
+- Member since
+- Roles
+- Note
+- Send message input
+- Basic JavaSript for the demo
+
+## Preview :
+
+<img src="https://i.imgur.com/gFLihvQ.png">
+
+Demo : <a href="https://sybrax.github.io/Discord-User-Card/">here</a>
+
+Download : <a href="https://github.com/Sybrax/Discord-User-Card/releases/tag/v2.0.0">here</a>
+
+## Old version preview :
+
+<img src="https://i.imgur.com/s2hTmWR.png">
+
+Download : <a href="https://github.com/Sybrax/Discord-User-Card/releases/tag/v1.0.0">here</a> ( Non-optimized code )
+
+## Community :
+
+Join creators-area to see more of the community: : <a href="https://discord.gg/fHYmhV3r3k">here</a>
+
+## Author :
+
+<p>
+  <a href="https://github.com/Sybrax"><img width="45" src="https://avatars.githubusercontent.com/u/45593750?s=64&v=4" alt="Lohan" style="max-width: 100%;"></a>
+</p>
+ 
+</ul>
